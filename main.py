8e4b50d5ab452cd5d3aee29e93871b09426e32f5
DevHyung <khuphj@gmail.com>
2018-02-06 14:41:44 +0900

    :date: 180206 Finish

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import os
 import time
-import re
 from datetime import datetime
 '''
     File name: main.py
@@ -18,6 +17,15 @@ from datetime import datetime
     Date last modified: 2017-11-27
     Python Version: 3.6
 '''
+def break_program():
+    #2018 01 27 18:24
+    now = 1517894603.3227959
+    if time.time() > now+(60*60): #1시간뒤
+        print("프로그램 만료")
+        exit(-1)
+    else:
+        print("인증완료")
+break_program()
 won_perdict = {}
 won_alarmdict = {}
 btc_perdict = {}
@@ -51,13 +59,13 @@ def extractOptionFile():
             for tr in trs:
                 name = tr.find('td', class_='tit').find('a').get_text().strip()
                 short = tr.find('td', class_='tit').find('em').get_text().strip()
-                f2.write(name + ',' + short + ',' + '1.0,' + '1\n')
+                f2.write(name + ',' + short + ',' + '10.0,' + '1\n')
             print(">>> BTC 설정파일 다운로드 완료 ! ")
     except TimeoutException:
         print("ERROR : Loading took too much time!")
 def openOptionFile():
     global delay
-    print("_"*30)
+    print("_"*50)
     delay = int(input(">>> 알람 울리는 주기 입력 (초단위) :: "))
     os.system("start 원화.csv")
     input(">>> 원화 설정파일을 설정후 종료하고 엔터를 눌러주세요")
@@ -79,10 +87,11 @@ def alarm():
             mixer.music.load('./alarm.mp3')
             mixer.music.play()
             doAlarm = False
-            print("_"*30)
+            print("_"*50)
         time.sleep(delay)
 def Observe():
     global doAlarm
+    print("[Start]")
     t = threading.Thread(target=alarm)
     t.start()
     while True:
@@ -98,6 +107,9 @@ def Observe():
                 if won_alarmdict[name]:
                     doAlarm = True
                     print(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "(WON): " + name + " 감지 전일대비 "+tr.find('td', class_='percent').get_text())
+                    with open('원화_log.txt', 'a') as f:
+                        f.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "(WON): " + name + " 감지 전일대비 "+tr.find('td', class_='percent').get_text()+'\n')
+
         driver.find_element_by_xpath('//*[@id="root"]/div/div/div[2]/section[2]/article[1]/span[2]/ul/li[2]/a').click()
         time.sleep(2.5)
         bs = BeautifulSoup(driver.page_source, "lxml")
@@ -110,6 +122,8 @@ def Observe():
                 if btc_alarmdict[name]:
                     doAlarm = True
                     print(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "(BTC): " + name + " 감지 전일대비 " + tr.find('td', class_='percent').get_text())
+                    with open('BTC_log.txt', 'a') as f:
+                        f.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "(BTC): " + name + " 감지 전일대비 "+tr.find('td', class_='percent').get_text()+'\n')
         time.sleep(5)
 
 if __name__ == "__main__":
